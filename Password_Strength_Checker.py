13432deef73e2105cfb25b9e330c976eb737b5da
Thomas Martin <87400753+tm4rtin17@users.noreply.github.com>
2025-01-06 19:32:01 -0800

    Update Password_Strength_Checker.py
    Updated code comments to reflect recent updates.

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,13 +1,20 @@
 import pyinputplus as pyip
+import sys
+import time
 
 #User inputs a password
 def input_password():
-    password = pyip.inputPassword("Enter the password you would like to verify.")
+    password = pyip.inputPassword("Enter the password you would like to verify. [Enter 'q' at to exit program.]")
     special_char = "!@#$%^&*()-+?_=,<>/"
     pass_length = len(password)
     contains_upper = any(char.isupper() for char in password)
     contains_special = any(char in special_char for char in password)
-    return pass_length, contains_upper, contains_special
+    if password == 'q': #Ends program
+        print("See you next time!")
+        time.sleep(1)
+        sys.exit()
+    else:
+        return pass_length, contains_upper, contains_special
 
 #flow control for password requirements
 def pass_strength(pass_length, contains_upper, contains_special):
@@ -45,8 +52,9 @@ pass_results(rating, pass_length, contains_upper, contains_special)
 
 #While loop for re-runs
 while True:
-    run_again = pyip.inputYesNo("Would you like to check another password or retry? (y/n)")
+    run_again = pyip.inputYesNo("Would you like to check another password? (y/n)")
     if run_again == "yes":
+        #Runs the same 3 functions as the initial calling
         pass_length, contains_upper, contains_special = input_password()
         rating = pass_strength(pass_length, contains_upper, contains_special)
         pass_results(rating, pass_length, contains_upper, contains_special)
