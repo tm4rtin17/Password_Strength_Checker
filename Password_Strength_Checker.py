46149dba628851dfeea00dfc6f847ccfaa62aa51
Thomas Martin <87400753+tm4rtin17@users.noreply.github.com>
2025-01-06 19:05:22 -0800

    Update Password_Strength_Checker.py
    - Separated check_password() into three different functions to simply code.
    - Simplified and shortened variables.

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,46 +1,55 @@
 import pyinputplus as pyip
 
-#Define check password function
-def check_password():
+#User inputs a password
+def input_password():
     password = pyip.inputPassword("Enter the password you would like to verify.")
-    special_characters = "!@#$%^&*()-+?_=,<>/"
-    password_length = len(password)
-    password_contains_uppercase = any(char.isupper() for char in password)
-    password_contains_special = any(char in special_characters for char in password)
-    password_strength = ""
-    
-    #flow control for password requirements
-    if password_length < 6:
-        password_strength = "weak"
-    elif password_length > 6 and password_length < 8:
-        password_strength = "average"
-    elif password_length >= 8 and password_length < 12 and password_contains_uppercase == True and password_contains_special == True:
-        password_strength = "strong"
-    elif password_length >= 12 and password_contains_uppercase == True and password_contains_special == True:
-        password_strength = "very strong"
+    special_char = "!@#$%^&*()-+?_=,<>/"
+    pass_length = len(password)
+    contains_upper = any(char.isupper() for char in password)
+    contains_special = any(char in special_char for char in password)
+    return pass_length, contains_upper, contains_special
+
+#flow control for password requirements
+def pass_strength(pass_length, contains_upper, contains_special):
+    if pass_length < 6:
+        rating = "weak"
+    elif pass_length > 6 and pass_length < 8:
+        rating = "average"
+    elif pass_length >= 8 and pass_length < 12 and contains_upper == True and contains_special == True:
+        rating = "strong"
+    elif pass_length >= 12 and contains_upper == True and contains_special == True:
+        rating = "very strong"
     else:
-        password_strength = "average"
+        rating = "average"
+    return rating
+
+#Returns results to the user
+def pass_results(rating, pass_length, contains_upper, contains_special):
 
     #print results of password strength
-    print(f"Your password is {password_strength}")
+    print(f"Your password is {rating}")
     
     #if statements to give user reccomendations for how to improve their password
-    if password_length < 12:
-        length_delta = 12 - password_length
+    if pass_length < 12:
+        length_delta = 12 - pass_length
         print(f"- Increase password length by {length_delta} characters.")
-    if password_contains_uppercase == False:
+    if contains_upper == False:
         print("- Add an uppercase letter.")
-    if password_contains_special == False:
+    if contains_special == False:
         print("- Add a special character or symbol '!@#$%^&*()-+?_=,<>/.'")
     
 #Initial Function Call
-check_password()
+pass_length, contains_upper, contains_special = input_password()
+rating = pass_strength(pass_length, contains_upper, contains_special)
+pass_results(rating, pass_length, contains_upper, contains_special)
 
 #While loop for re-runs
 while True:
     run_again = pyip.inputYesNo("Would you like to check another password or retry? (y/n)")
     if run_again == "yes":
-        check_password()
+        pass_length, contains_upper, contains_special = input_password()
+        rating = pass_strength(pass_length, contains_upper, contains_special)
+        pass_results(rating, pass_length, contains_upper, contains_special)
     else:
         print("Thanks for keeping your passwords secure!")
         break
